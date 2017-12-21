71f3e7a1c070726dd017f9219d658e1575c1eb11
maidou <maidou@anybeen.net>
2017-12-21 14:30:43 +0800

    statistics 统计 payload 负载 coordinate 坐标 rate 率 separate capacity 容量 relevant 相关的 massive 大量的

--- app/src/main/java/com/dyzs/okhttpsample/OkHttpHelper.java
+++ app/src/main/java/com/dyzs/okhttpsample/OkHttpHelper.java
@@ -1,10 +1,18 @@
 package com.dyzs.okhttpsample;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -24,9 +32,31 @@ public class OkHttpHelper {
     private static OkHttpHelper mInstance;
     private final OkHttpClient mOkHttpClient;
 
+    // see MultipartBody
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+
+    public static final MediaType MIXED = MediaType.parse("multipart/mixed");
+    public static final MediaType ALTERNATIVE = MediaType.parse("multipart/alternative");
+    public static final MediaType DIGEST = MediaType.parse("multipart/digest");
+    public static final MediaType PARALLEL = MediaType.parse("multipart/parallel");
+    public static final MediaType FORM = MediaType.parse("multipart/form-data");
+
+
+
     private OkHttpHelper() {
         mOkHttpClient = new OkHttpClient.Builder()
+                /*.cookieJar(new CookieJar() {
+                    @Override
+                    public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+
+                    }
+
+                    @Override
+                    public List<Cookie> loadForRequest(HttpUrl url) {
+                        return null;
+                    }
+                })*/
                 .connectTimeout(3, TimeUnit.SECONDS)
                 .readTimeout(30, TimeUnit.SECONDS)
                 .writeTimeout(30, TimeUnit.SECONDS)
@@ -50,10 +80,16 @@ public class OkHttpHelper {
      * response.body().byteStream();
      * @param url
      * @param iCallBack
+     *
+     *
+     * 下载一个文件，打印他的响应头，以string形式打印响应体。
+     * 响应体的 string() 方法对于小文档来说十分方便、高效。但是如果响应体太大（超过1MB），应避免适应 string()方法 ，因为他会将把整个文档加载到内存中。
+     * 对于超过1MB的响应body，应使用流的方式来处理body。
      */
-    public void pullStringMethodGet(String url, final ICallBack iCallBack) {
+    public void getStringMethodGet(String url, final ICallBack iCallBack) {
         Request request = new Request.Builder()
                 .url(url)
+                //.header("Cookie", "xxx")
                 .build();
         Call call = mOkHttpClient.newCall(request);
 
@@ -79,7 +115,40 @@ public class OkHttpHelper {
     }
 
 
-    public void pushJsonMethodPost(String url, String json) throws IOException{
+    public void getStringHeaderMethodGet(String url, final ICallBack iCallBack) {
+        Request request = new Request.Builder()
+                .url(url)
+                //.header("Cookie", "xxx")
+                .build();
+        Call call = mOkHttpClient.newCall(request);
+
+        try {
+
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    iCallBack.onFailure(call, e);
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        iCallBack.onSuccess(response.body().string());
+                    }
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0; i < responseHeaders.size(); i++) {
+                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            iCallBack.onFailure(e.getMessage());
+        }
+    }
+
+
+    public void postJsonMethodPost(String url, String json) throws IOException{
         RequestBody body = RequestBody.create(JSON, json);
         Request request = new Request.Builder()
                 .url(url)
@@ -105,7 +174,47 @@ public class OkHttpHelper {
     }
 
 
+    public void postKeyValueMethodPost(String url, String json) throws IOException {
+        RequestBody requestBody = new FormBody.Builder()
+                .add("platform", "android")
+                .add("username", "bug")
+                .add("subject", "osChina")
+                .build();
 
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+        Call call = mOkHttpClient.newCall(request);
+        try {
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        //response.body().string();
+                    } else {
+                        // throw new IOException("Unexpected code " + response);
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 基于 Http 的文件上传
+     *
+     */
+    public void uploadFile(File file) {
+
+    }
 
 
 
